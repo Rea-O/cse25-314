faf298d2846a643a1b0ad09d9eff29572e21481d
Rea-O <osiamerathapo7@gmail.com>
2026-05-26 09:57:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    const subForm = document.getElementById('subForm');
-    if (subForm) {
-        subForm.addEventListener('submit', (e) => {
-            e.preventDefault();
-            alert('Thank you! Skeif Botsogo will contact you shortly to confirm your order.');
-        });
-    }
-
     const questionForm = document.getElementById('questionForm');
     if (questionForm) {
         questionForm.addEventListener('submit', (e) => {
